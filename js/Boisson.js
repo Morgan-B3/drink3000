8858e3b0e4fb4432a8fda2ff6ab012b0fbe155aa
Morgan-B3 <boutroism@gmail.com>
2023-12-13 14:18:02 +0100

    ADDED JS : class Boisson

---
+++ js/Boisson.js
@@ -0,0 +1,9 @@
+export default class Boisson {
+
+    constructor(name, description, type, color){
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.color = color;
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,24 @@
+import Boisson from "./Boisson.js";
+
+/**
+ * Recettes
+ */
+let boissons = [];
+
+boissons.push(new Boisson(
+    "Original",
+    "Boisson énergisante gazeuse, à la taurine et chargée en caféine",
+    "energisante.",
+    "red"));
+
+boissons.push(new Boisson(
+    "Mojito",
+    "Boisson énergisante gazeuse, saveur mojito à la caféine et sans alcool",
+    "energisante.",
+    "green"));
+
+boissons.push(new Boisson(
+    "Tropical",
+    "Boisson énergisante gazeuse, aux fruits, taurine, caféine et extrait de gingembre.",
+    "energisante",
+    "orange"));
